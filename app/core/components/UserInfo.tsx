4fcd67e2868512f204e011eff7f852b236cb59aa
kijikunnn <kijikunnn@gmail.com>
2021-12-10 17:13:11 +0900

    🎨 共通ボタンのコンポーネント化

--- app/core/components/UserInfo.tsx
+++ app/core/components/UserInfo.tsx
@@ -1,29 +1,37 @@
 import logout from "app/auth/mutations/logout"
-import updateName from "app/users/mutations/updateName"
-import updateTime from "app/users/mutations/updateTime"
 import { Link, Image, useMutation } from "blitz"
-import React, { useEffect, VFC } from "react"
+import React, { VFC } from "react"
 import { useCurrentUser } from "../hooks/useCurrentUser"
 import GoogleLogin from "public/btn_google_signin_dark_normal_web@2x.png"
+
+type LINK_PROPS = {
+  title: string
+  href: string
+}
 
 export const UserInfo: VFC = () => {
   const currentUser = useCurrentUser()
   const [logoutMutation] = useMutation(logout)
 
+  const LinkButton: VFC<LINK_PROPS> = (props) => {
+    return (
+      <Link href={props.href}>
+        <a className="bg-yellow-200 block m-4 w-80 py-3 rounded-lg text-3xl font-bold text-gray-900 text-center">
+          {props.title}
+        </a>
+      </Link>
+    )
+  }
+
   if (currentUser) {
     return (
       <div className="flex flex-col">
         {/* TODO: ボタンをコンポーネント化 */}
-        <Link href="/game">
-          <a>Game Start</a>
-        </Link>
-        <Link href="/score">
-          <a>Score</a>
-        </Link>
-        <Link href="/profile">
-          <a>Profile</a>
-        </Link>
+        <LinkButton title="Game Start" href="/game" />
+        <LinkButton title="Score" href="/score" />
+        <LinkButton title="Profile" href="/profile" />
         <button
+          className="bg-yellow-200 block m-4 w-80 py-3 rounded-lg text-3xl font-bold text-gray-900"
           onClick={async () => {
             await logoutMutation()
           }}
