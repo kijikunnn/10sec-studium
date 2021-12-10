71fae356e4acf3b77d120bb518640cfa36d4b96e
kijikunnn <kijikunnn@gmail.com>
2021-12-10 18:21:07 +0900

    🎨 タイムのフェードアウト実装

--- app/pages/game.tsx
+++ app/pages/game.tsx
@@ -22,13 +22,15 @@ const Game: BlitzPage = () => {
   }
 
   const handleStop = async () => {
+    if (!currentUser) return
+
     window.clearInterval(timer)
     setIsStart((prevIsStart) => prevIsStart + 1)
 
     const currentTime = parseFloat(time.toFixed(2))
     const currentError = Math.abs(parseFloat((currentTime - 10).toFixed(2)))
 
-    if (currentUser?.error && currentUser?.error > currentError) {
+    if (currentUser.error === null || currentUser.error > currentError) {
       setIsNewRecord(true)
       await updateTimeMutation({ ...currentUser, time: currentTime, error: currentError })
     }
@@ -37,10 +39,18 @@ const Game: BlitzPage = () => {
   return (
     <>
       <div className="h-2/5 flex flex-col items-center justify-end">
-        {isNewRecord && <div className="text-7xl text-red-500 font-bold mb-16">New Record</div>}
+        {isNewRecord && <div className="text-7xl text-red-500 font-bold mb-4">New Record</div>}
         <div className="bg-[#FFEE00] h-48 w-full rounded-2xl flex items-center">
           <div className="bg-black m-4 w-full rounded-2xl">
-            <p className="text-center text-[#FFEE00] text-[100px] mx-auto">{time.toFixed(2)}</p>
+            <p
+              className={
+                isStart === 1
+                  ? "text-center text-[#FFEE00] text-[100px] mx-auto  transition duration-[5000ms] opacity-0"
+                  : "text-center text-[#FFEE00] text-[100px] mx-auto opacity-1"
+              }
+            >
+              {time.toFixed(2)}
+            </p>
           </div>
         </div>
       </div>
