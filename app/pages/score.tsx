4f6399b6ed16ca2342d68318c1abfacfee196300
kijikunnn <kijikunnn@gmail.com>
2021-12-10 19:40:13 +0900

    scoreページの実装

--- app/pages/score.tsx
+++ app/pages/score.tsx
@@ -1,8 +1,25 @@
-import { BlitzPage, Link } from "blitz"
+import { BlitzPage, Link, useQuery } from "blitz"
 import Layout from "app/core/layouts/Layout"
 import { TitleWithLink } from "app/core/components/TitleWithLink"
+import getUsers from "app/users/queries/getUsers"
+import { useCurrentUser } from "app/core/hooks/useCurrentUser"
 
 const Score: BlitzPage = () => {
+  const currentUser = useCurrentUser()
+  const [{ users, ids }] = useQuery(getUsers, { orderBy: { error: "asc" } })
+
+  const myRank = ids.indexOf(currentUser?.id) + 1
+
+  const editError = (time: number, error: number): string => {
+    if (error === 0) {
+      return "±0.00"
+    } else if (time < 10) {
+      return `-${error}`
+    } else {
+      return `+${error}`
+    }
+  }
+
   return (
     <>
       <TitleWithLink title="Ranking" />
@@ -11,26 +28,31 @@ const Score: BlitzPage = () => {
         <ul className="flex justify-around">
           <li className="flex items-baseline">
             <div className="mr-1">My Best Score:</div>
-            <div className="text-3xl font-bold">9.98</div>
+            <div className="text-3xl font-bold">{currentUser?.time}</div>
           </li>
           <li className="flex items-end">
-            <div className="text-3xl font-bold">圏外</div>
+            <div className="text-3xl font-bold">{myRank ? myRank : "圏外"}</div>
             <div> / 50位</div>
           </li>
         </ul>
       </div>
-
-      <div className="flow-root text-lg">
-        <div className="p-2 m-3 bg-yellow-200 rounded-lg text-3xl">
-          <ul className="flex  justify-around">
-            1.<li className="font-bold">佐藤さん</li>
-            <li className="flex items-baseline">
-              <div className="mr-3 font-bold">10.000</div>
-              <div className="text-2xl ">±0.000</div>
-            </li>
-          </ul>
-        </div>
-      </div>
+      <ul className="overflow-auto sm:h-[calc(100vh-25%-72px)]">
+        {users.map((user, i: number) => (
+          <li
+            key={user.id}
+            className="flex justify-around p-2 m-3 bg-yellow-200 rounded-lg text-3xl font-bold"
+          >
+            <p className="font-medium">{i + 1}.</p>
+            <p>{user.name}</p>
+            <p className="flex items-baseline">
+              <p className="mr-3 ">{user.time ? user.time : "No time"}</p>
+              <p className="font-medium text-2xl ">
+                {user.error ? editError(user.time, user.error) : "-"}
+              </p>
+            </p>
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
