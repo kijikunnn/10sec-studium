35e97068cfd94e7875b964a750afb365db6dd008
kijikunnn <kijikunnn@gmail.com>
2021-12-11 12:59:30 +0900

    💄 Loadingアニメーションのレスポンシブ対応

--- app/core/components/Loading.tsx
+++ app/core/components/Loading.tsx
@@ -4,7 +4,7 @@ export const Loading: VFC = () => {
   return (
     <div className="flex justify-center items-center h-screen">
       <div
-        className="animate-spin h-10 w-10 border-4 border-yellow-500 rounded-full"
+        className="animate-spin h-16 w-16 border-8 sm:h-10 sm:w-10 sm:border-4 border-yellow-500 rounded-full"
         style={{ borderTopColor: "transparent" }}
       ></div>
     </div>
